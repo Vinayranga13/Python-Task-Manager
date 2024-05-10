20f370aa3ff7ffb602c43c17ac1d2685fe1f0ca9
Vinayranga13 <vinayranga502@gmail.com>
2024-05-10 16:24:58 +0530

    Changed models to django DB and finished all DB creations

---
+++ views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
+
+# Display the list of tasks
+def task_list(request):
+    tasks = Task.objects.all().order_by('due_date')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+# Task detail view
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'tasks/task_detail.html', {'task': task})
+
+# Add a new task
+def task_add(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+# Edit an existing task
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, request.FILES, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
