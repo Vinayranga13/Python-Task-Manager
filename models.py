b92b99779ff6985e8e8fbfeaef4296ba54c0672a
Vinayranga13 <vinayranga502@gmail.com>
2024-05-10 16:06:22 +0530

    Remove auth.py as it was useless, Also changed the templates to match the changes (Changed from using Flask to using Django. Why?- Because i had a tough time fixing the Flask WTF forms and The lack of time to study more about WTF Flask forms)

--- models.py
+++ models.py
@@ -1,20 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from datetime import datetime
+from django.contrib import admin
+from .models import Task
 
-db = SQLAlchemy()
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    name = db.Column(db.String(150), nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    due_date = db.Column(db.DateTime, nullable=True)
-    image = db.Column(db.LargeBinary, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship("User", backref="tasks")
+admin.site.register(Task)
